88986c41d93ca5f5b0c6a476fc4f9889f5558534
menja-coder <rakotomenja@gmail.com>
2019-09-12 16:03:15 +0300

    controller reservation done

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,11 +9,24 @@ class ReservationsController < ApplicationController
 
   end
 
-  def create
-  end
 
   def new
+    @reservation = Reservation.new
   end
+
+  def create
+    @reservation = Reservation.new(nbr_person: params[:nbr_person],
+                                   date: params[:date],
+                                   client_id: current_client.id,
+                                   restaurant_id: session[:resto_id])
+    if @reservation.save
+      redirect_to root_path
+    else
+
+    end
+
+  end
+
 
   def update
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :clients
+  
   resources :reservations
 
   root 'restaurants#index'
