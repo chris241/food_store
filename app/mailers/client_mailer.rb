097d35cfaf398a5511d0561cca56e91880f845fb
AroRk226 <arorakotonindrina12@gmail.com>
2019-09-12 14:27:26 +0300

    Action mailer

---
+++ app/mailers/client_mailer.rb
@@ -0,0 +1,14 @@
+class ClientMailer < ApplicationMailer
+  default from: 'no-reply@FoodStore.mg'
+ 
+  def welcome_email(client)
+    #on récupère l'instance client pour ensuite pouvoir la passer à la view en @client
+    @client = client 
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'https://www.FoodStore.mg' 
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @client.email, subject: 'Bienvenue chez nous !')
+  end
+end
--- app/models/client.rb
+++ app/models/client.rb
@@ -7,4 +7,11 @@ class Client < ApplicationRecord
   has_many :reservations
   has_many :commands
   has_many :restaurants, through: :reservations
+
+  after_create :welcome_send
+
+  def welcome_send
+    ClientMailer.welcome_email(self).deliver_now
+  end
+  
 end
