7c46be675f0f5a6166167bd830b8e768d78f844a
Nam-9 <naninambinina@gmail.com>
2019-09-12 14:08:38 +0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Food.destroy_all
+Menu.destroy_all
+Restaurant.destroy_all
+
+require 'faker'
+
+5.times do 
+	restaurant = Restaurant.create(name: Faker::Restaurant.name,
+		                           address:Faker::Address.street_address,
+		                           description:Faker::Restaurant.description,
+		                           image_url:"#{Faker::Restaurant.name}@gmail.com"
+
+
+		)
+	
+end
+
+ 5.times do
+ 	category =["Dessert","Entrée","Résistance","Boisson"]
+     for i in (0..category.length-1)
+ 	 menu = Menu.create(category:category[i])
+ 	end
+end	
+
+
+
+5.times do
+	food =Food.create!(name:Faker::Food.dish,
+		              description:Faker::Food.description,
+		              duration:rand(1..60),
+		              price:Faker::Commerce.price,
+		              menu:Menu.all.sample
+		                 )
+end	
+
+puts "resto"
+puts "menu"
+puts "food"
