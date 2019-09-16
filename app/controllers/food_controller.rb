583460bacc1b34cd65c18cc282e6c2c34aea5fc8
Nam-9 <naninambinina@gmail.com>
2019-09-16 09:27:09 +0300

    restoavatar

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -1,17 +1,2 @@
 class FoodController < ApplicationController
-	def index
-		@foods = Food.all
-	end 
-	def show
-     @food = Food.find(params[:id])
-    session[:food_id]=params[:id]
-	end	
-	def create
-		food = Food.create(name: params[:name][0],
-			               duration: params[:address][0],  
-			               price:params[:price],
-			               description: params[:description][0],
-			               image_url:params[:image_url]
-			                )
-	end	
 end
