8d119c77c608e160b2b449bb17636e510bfb4494
chris241 <chrisleon325@gmail.com>
2019-09-16 11:59:17 +0300

    heroku v1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,5 @@ config.assets.precompile += %w[active_admin.css active_admin.js]
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 } [ config.action_mailer.default_url_options = {host: 'hôte local', port: 3000}] 
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { :host => 'https://foodsstore.herokuapp.com/' }
-  config.action_mailer.perform_deliveries = true
+ 
 
 end
