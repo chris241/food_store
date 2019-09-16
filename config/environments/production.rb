e7c94b72ab3cae2e236008f4eefb66ed49705bbd
chris241 <chrisleon325@gmail.com>
2019-09-16 11:53:53 +0300

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,6 +112,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = { :host => 'https://foodsstore.herokuapp.com/' }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
 end
