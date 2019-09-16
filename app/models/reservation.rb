b4273c78cd349ce1ec8d47dc98f1027e4ef9f114
chris241 <chrisleon325@gmail.com>
2019-09-16 10:40:03 +0300

    validation restaurant create admin

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -9,4 +9,12 @@ class Reservation < ApplicationRecord
   def reservation_send
     ClientMailer.reservation_email(self).deliver_now
   end
+  validates :nbr_person, presence: true, length: {in:1..7}
+  validates :date, presence: true 
+  validate :date_cannot_be_in_the_past
+  def date_cannot_be_in_the_past
+      if date < Date.today
+            errors.add(:date, "can't be in the past")
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :reservations
   root 'restaurants#index'
   devise_for :clients
