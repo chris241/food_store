8ac5d3284354e35c1c2f8ff8a342710c9cd55baa
Nam-9 <naninambinina@gmail.com>
2019-09-16 10:27:36 +0300

    maj

--- app/controllers/foodavatar_controller.rb
+++ app/controllers/foodavatar_controller.rb
@@ -1,8 +1,6 @@
 class FoodavatarController < ApplicationController
 	def create
-	@food = Food.find(params[:food_id])
-    @food.foodavatar.attach(params[:foodavatar])
-    redirect_to(food_path(@food))
+	
     end
 
 end
