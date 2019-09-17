3c532653de0c155b7dd9b87701c7be3972303385
AroRk226 <arorakotonindrina12@gmail.com>
2019-09-17 10:33:16 +0300

    mailer

--- config/environment.rb
+++ config/environment.rb
@@ -1,40 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
 
-setx -m $RECIPIENT_EMAIL "current_client.email"
-
 # Initialize the Rails application.
 Rails.application.initialize!
-require 'mailjet'
-Mailjet.configure do |config|
-config.api_key = '67bc69ef6c6e913806f8e378467143fa'
-config.secret_key = '736f7480d41ae481568b42907791e6dd'
-config.api_version = "v3.1"
-end
-variable = Mailjet::Send.create(messages: [{
-  'From'=> {
-    'Email'=> 'aomine280498@gmail.com',
-    'Name'=> 'Aro'
-  },
-  'To'=> [
-    {
-      'Email' => "$RECIPIENT_EMAIL",
-      'Name' => "You"
-    }
-  ],
-  'Subject'=> 'Greetings from Mailjet.',
-  'TextPart'=> 'My first Mailjet email',
-  'HTMLPart'=> '<h3>Cher utilisateur, bienvenue sur notre site <a href=\'https://www.foodsstore.herokuapp.com/\'>Foodsstore</a>!</h3><br />Nous sommes impatients de vous revoir à très bientôt!',
-  'CustomID' => 'AppGettingStartedTest'
-}]
-)
-
-ActionMailer::Base.smtp_settings =   {
-    :address            => 'in-v3.mailjet.com',
-    :port               => 25,
-    :domain             => 'gmail.com', #you can also use google.com
-    :authentication     => :plain,
-    :user_name          => ENV['MAILJET_LOGIN'],
-    :password           => ENV['MAILJET_PWD']
-  }
-p variable.attributes['Messages']
