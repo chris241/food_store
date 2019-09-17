1f174e84bb6ec3f18f30381db9683e2d92d20af0
Nam-9 <naninambinina@gmail.com>
2019-09-17 16:51:21 +0300

    maj

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,15 +1,16 @@
 class RestaurantsController < ApplicationController
   def index
-  	@restaurants = Restaurant.all
+    @restaurants = Restaurant.all
+    
   end
 
   def show
-  	@restaurant = Restaurant.find(params[:id])
+    @restaurant = Restaurant.find(params[:id])
     session[:resto_id]=params[:id]
   end
 
   def create
-  	 @restaurant = Restaurant.create(name: params[:name][0],
+     @restaurant = Restaurant.create(name: params[:name][0],
                                   address: params[:address][0],
                                   description: params[:description][0],
                                   image_url:params[:image_url]
@@ -20,8 +21,24 @@ class RestaurantsController < ApplicationController
 
      end
   end 
+  def update
+    @restaurant = Restaurant.find(params[:id])
 
-
-  
+    if @restaurant.update(name: params[:name][0],
+                          address: params[:address][0],
+                          description: params[:description][0],
+                           image_url:params[:image_url]
+                                  )
+        flash[:success] = "Votre restaurant a été édité!"
+        redirect_to root_path
+    else
+      render :edit
+    end
+  end  
+  def destroy
+   @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to root_path
+  end  
 
 end
