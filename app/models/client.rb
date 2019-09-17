c0f34074fb0b9ffe6a7256381ca728144aaf1967
chris241 <chrisleon325@gmail.com>
2019-09-17 13:09:25 +0300

    maj

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,13 +2,9 @@ class Client < ApplicationRecord
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  after_create :welcome_send
-  def welcome_send
-    ClientMailer.welcome_email(self).deliver_now
-  end
+ 
    has_one_attached :avatar
   has_many :reservations
   has_many :commands
