3d8ee0973d66c8042829ff27b9f496e7b2c08e99
chris241 <chrisleon325@gmail.com>
2019-09-17 11:20:55 +0300

    maj

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,12 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings =   {
-    :address            => 'smtp.gmail.com',
-    :port               => 25,
-    :domain             => 'gmail.com', #you can also use google.com
-    :authentication     => :plain,
-    :user_name          => "#{ENV['GMAIL_LOGIN']}",
-    :password           => "#{ENV['GMAIL_PWD']}",
-    :enable_starttls_auto => true
-  }
+config.action_mailer.smtp_settings = {  
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "foodsstore.herokuapp.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+}
