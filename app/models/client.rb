3f2eafbcfd02fd06e214fa97c0f1fe89ceb9338a
chris241 <chrisleon325@gmail.com>
2019-09-17 10:14:23 +0300

    mailer

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,11 @@
 class Client < ApplicationRecord
+  after_create :welcome_send
 
+  def welcome_send
+
+    ClientMailer.welcome_email(self).deliver_now
+
+  end
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
