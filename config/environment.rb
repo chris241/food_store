0aa09eddeb79a1de21edfd96c81ad9449f449dcc
chris241 <chrisleon325@gmail.com>
2019-09-17 12:05:30 +0300

    m

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_LOGIN'],
   :password => ENV['SENDGRID_PWD'],
-  :domain => 'monsite.fr',
+  :domain => 'gmail.com',
   :address => 'smtp.sendgrid.net',
   :port => 25,
   :authentication => :plain,
