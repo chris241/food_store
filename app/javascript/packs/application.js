da7a01f11941a0556309bf5bc75c31b6fe6287dd
tsiryJL <tsiry.razanakoto@gmail.com>
2019-09-17 23:14:18 +0300

    remake view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+require('packs/test.js')
 
 //= require jquery-1.10.2.min
 //= require js/bootstrap.min
