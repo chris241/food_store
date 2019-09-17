b4b0ae94608b44fb3c687797d667aadebdf01c5d
chris241 <chrisleon325@gmail.com>
2019-09-17 11:41:25 +0300

    a

--- config/environment.rb
+++ config/environment.rb
@@ -2,11 +2,12 @@
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings =   {
-    :address            => 'smtp.gmail.com',
-    :port               => 587,
-    :domain             => 'gmail.com', #you can also use google.com
-    :authentication     => :plain,
-    :user_name          => ENV["GMAIL_USERNAME"],
-    :password           => ENV["GMAIL_PASSWORD"]
-  }
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_LOGIN'],
+  :password => ENV['SENDGRID_PWD'],
+  :domain => 'monsite.fr',
+  :address => 'smtp.sendgrid.net',
+  :port => 25,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
