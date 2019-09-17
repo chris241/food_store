a43c2513911e35175217786a9c79a4c62ed6933e
AroRk226 <arorakotonindrina12@gmail.com>
2019-09-17 15:23:23 +0300

    Mailer

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module FoodStore
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -19,3 +18,4 @@ module FoodStore
     # the framework and any gems in your application.
   end
 end
+config.assets.initialize_on_precompile = false
