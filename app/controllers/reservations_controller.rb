ed297538afa85c4e101dfad0eabe5a49219261b2
AroRk226 <arorakotonindrina12@gmail.com>
2019-09-18 16:12:07 +0300

    edit

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -21,9 +21,6 @@ class ReservationsController < ApplicationController
                                    client_id: current_client.id,
                                    restaurant_id: session[:resto_id])
 
-    @listes = JoinResFood.new(reservation_id: current_client.command.id ,food_id: @food_id)
-
-
     if @reservation.save
       flash[:success] = "Vous avez enregistré une réservation"
       redirect_to reservations_path
