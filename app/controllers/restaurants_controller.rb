8549b4df985af5ee6943051240b7cd04c36efac1
chris241 <chrisleon325@gmail.com>
2019-09-19 09:05:10 +0300

    barre de recherche

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,8 @@
 class RestaurantsController < ApplicationController
   def index
-  	@restaurants = Restaurant.all
+    @q = Restaurant.search(params[:q])
+  @restaurants = @q.result
+  
   end
 
   def show
