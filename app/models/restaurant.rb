148a7564cb110c816d578d76fe13779cf30b19f8
Nam-9 <naninambinina@gmail.com>
2019-09-21 15:59:06 +0300

    modif seed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,6 @@ class Restaurant < ApplicationRecord
 	has_many :reservations
 	has_many :clients, through: :reservations
 	has_one_attached :restoavatar
-	belongs_to :gerant
+	belongs_to :gerant , optional: true
 	
 end
