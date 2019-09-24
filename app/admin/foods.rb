bcb966f4a02daaef7340b2fae9cfb0182a87a9ab
chris241 <chrisleon@325.gmail.com>
2019-09-24 23:20:55 +0300

    maj

--- app/admin/foods.rb
+++ app/admin/foods.rb
@@ -1,16 +1,16 @@
-ActiveAdmin.register Reservation do
+ActiveAdmin.register Food do
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :nbr_person, :date, :client_id, :restaurant_id
+  # permit_params :name, :description, :price, :duration, :image_url, :menu_id
   #
   # or
   #
   # permit_params do
-  #   permitted = [:nbr_person, :date, :client_id, :restaurant_id]
+  #   permitted = [:name, :description, :price, :duration, :image_url, :menu_id]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
--- app/admin/gerants.rb
+++ app/admin/gerants.rb
@@ -1,16 +1,16 @@
-ActiveAdmin.register Reservation do
+ActiveAdmin.register Gerant do
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :nbr_person, :date, :client_id, :restaurant_id
+  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
   #
   # or
   #
   # permit_params do
-  #   permitted = [:nbr_person, :date, :client_id, :restaurant_id]
+  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
